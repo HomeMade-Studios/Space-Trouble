3b28c16ec42502b69b0666ea321aade01b4e71c1
Romboide82 <ugo98.juve@libero.it>
2015-07-28 21:50:11 +0200

    n

--- Assets/Scripts/Spaceship.cs
+++ Assets/Scripts/Spaceship.cs
@@ -45,13 +45,6 @@ public class Spaceship : MonoBehaviour {
 		Time.timeScale = 5f;
 	}
 
-	void OnCollisionEnter2D(Collision2D other){
-		if(other.gameObject.tag == "Obstacle")
-			Destroy();
-		if(other.gameObject.tag == "DangerZone")
-			Time.timeScale = 1f;
-	}
-
 	void OnTriggerEnter2D(Collider2D other){
 		if(other.gameObject.tag == "Obstacle")
 			Destroy();
