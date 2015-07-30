9223f20d1da4222c5d68e38349618393d31f5dac
Giampli <france.marco@gmail.com>
2015-07-30 02:01:15 +0200

    LeCamere

--- Assets/Scripts/BackgroundRandomizer.cs
+++ Assets/Scripts/BackgroundRandomizer.cs
@@ -4,7 +4,7 @@ using System.Collections;
 public class BackgroundRandomizer : MonoBehaviour {
 
 	void Awake () {
-
+		this.transform.rotation = Quaternion.Euler (new Vector3 (Random.Range (0f, 360f), Random.Range (0f, 360f), Random.Range (0f, 360f)));
 	}
 
 }
