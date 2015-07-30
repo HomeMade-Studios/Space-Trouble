5fe791ad96b2acc273040e3a935fc8c006369f4a
Romboide82 <ugo98.juve@libero.it>
2015-07-30 22:09:47 +0200

    j

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -5,10 +5,16 @@ using System.Collections;
 public class MainMenu : MonoBehaviour {
 
 	public Text levelReached;
+	public GameObject startMessage;
 	public static int maxLevel = 10;
 
 	void Awake(){
-		//PlayerPrefs.DeleteAll(); ///ELIMINARE PER LA BUILD FINALE!!
+		//PlayerPrefs.DeleteAll();
+		if(PlayerPrefs.GetInt ("firstTime", 1) == 1){
+			PlayerPrefs.DeleteAll();
+			PlayerPrefs.SetInt("firstTime", 0);
+			ActiveStartMessage();
+		}
 		levelReached.text = (PlayerPrefs.GetInt("completedLevels", 0)).ToString() + " / " + maxLevel.ToString() + " km";
 	}
 
@@ -16,6 +22,10 @@ public class MainMenu : MonoBehaviour {
 		if(Input.GetKeyDown(KeyCode.Escape)){
 			Application.Quit();
 		}
+	}
+
+	public void ActiveStartMessage(){
+		startMessage.SetActive(true);
 	}
 
 	public void ToLevelSelection(){
