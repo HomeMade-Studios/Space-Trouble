932e045f403d447265e3c6a09d5884319294a50e
Romboide82 <ugo98.juve@libero.it>
2015-07-31 14:32:42 +0200

    c

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -40,12 +40,16 @@ public class MainMenu : MonoBehaviour {
 		welcomeMessagePanel.SetActive(false);
 	}
 
+	public void OpenGamePlayStore(){
+		Application.OpenURL("http://play.google.com/store/apps/dev?id=8610543020411511969");
+	}
+
 	public void OpenHomeMadeStudiosPlayStore(){
-		Application.OpenURL("https://play.google.com/store/apps/dev?id=8610543020411511969");
+		Application.OpenURL("http://play.google.com/store/apps/dev?id=8610543020411511969");
 	}
 
 	public void OpenAlexandrZhelanovSoundCloud(){
-		Application.OpenURL("https://soundcloud.com/alexandr-zhelanov");
+		Application.OpenURL("http://soundcloud.com/alexandr-zhelanov");
 	}
 
 	public void ToLevelSelection(){
