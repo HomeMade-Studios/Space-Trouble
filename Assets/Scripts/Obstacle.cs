b532d519f870b22138d1994d151f47fa571b5ff3
Romboide82 <ugo98.juve@libero.it>
2015-08-13 23:16:56 +0200

    n

--- Assets/Scripts/Obstacle.cs
+++ Assets/Scripts/Obstacle.cs
@@ -3,9 +3,31 @@ using System.Collections;
 
 public class Obstacle : MonoBehaviour {
 
-	public float rotationSpeed;
+	public float rotationSpeed, movementSpeed;
+	public Vector3 movementPoint1, movementPoint2;
+	bool isMovingToSecondPoint;
+
+	void Start () {
+		isMovingToSecondPoint = true;
+	}
 
 	void Update () {
-		transform.Rotate (Vector3.forward * rotationSpeed * Time.deltaTime);
+		if(rotationSpeed != 0){
+			transform.Rotate (Vector3.forward * rotationSpeed * Time.deltaTime);
+		}
+		if(movementSpeed != 0){
+			if(isMovingToSecondPoint){
+				transform.position = Vector3.MoveTowards(transform.position, movementPoint2, movementSpeed * Time.deltaTime);
+				if(transform.position == movementPoint2){
+					isMovingToSecondPoint = false;
+				}
+			}
+			else{
+				transform.position = Vector3.MoveTowards(transform.position, movementPoint1, movementSpeed * Time.deltaTime);
+				if(transform.position == movementPoint1){
+					isMovingToSecondPoint = true;
+				}
+			}
+		}
 	}
 }
