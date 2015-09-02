65017349720d827c1d0ce6e44182075435ae7dc0
Romboide82 <ugo98.juve@libero.it>
2015-09-02 16:05:18 +0200

    k

---
+++ Assets/Scripts/SoomlaStoreInitializer.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+using Soomla;
+
+namespace Soomla.Store.SpaceTrouble {
+
+	public class SoomlaStoreInitializer : MonoBehaviour {
+
+		void Start(){
+			SoomlaStore.Initialize(new SpaceTroubleAssets());
+		}
+
+		void Update () {
+			
+		}
+
+		public void PurchaseTest(){
+			try{
+				Debug.Log("Attempt to purchase");
+				StoreInventory.BuyItem("shield_pack_5", "0.70");
+				print ("c");
+			}
+			catch(Exception e){
+				Debug.Log("SOOMLA/UNITY" + e.Message);
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/SpaceTroubleAssets.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Soomla.Store.SpaceTrouble {
+
+	/// This class defines our game's economy, which includes virtual goods, virtual currencies
+	/// and currency packs, virtual categories
+	public class SpaceTroubleAssets : IStoreAssets{
+
+		public int GetVersion() {
+			return 0;
+		}
+
+		public VirtualCurrency[] GetCurrencies() {
+			return new VirtualCurrency[]{SHIELD_CURRENCY};
+		}
+
+		public VirtualGood[] GetGoods() {
+			return new VirtualGood[] {NO_ADS_LTVG};
+		}
+
+		public VirtualCurrencyPack[] GetCurrencyPacks() {
+			return new VirtualCurrencyPack[] {FIVESHIELD_PACK};
+		}
+
+		public VirtualCategory[] GetCategories() {
+			return new VirtualCategory[]{};
+		}
+		
+		/** Static Final Members **/
+
+		public const string SHIELD_CURRENCY_ITEM_ID = "shield";
+
+		public const string FIVESHIELD_PACK_PRODUCT_ID = "android.test.purchased";
+		
+		public const string NO_ADS_LIFETIME_PRODUCT_ID = "no_ads";	
+		
+		/** Virtual Currencies **/
+
+		public static VirtualCurrency SHIELD_CURRENCY = new VirtualCurrency(
+			"Shield",										// name
+			"Prevent to be destroyed",						// description
+			SHIELD_CURRENCY_ITEM_ID							// item id
+			);
+
+		/** Virtual Currency Packs **/
+		
+		public static VirtualCurrencyPack FIVESHIELD_PACK = new VirtualCurrencyPack(
+			"5 Shield",										// name
+			"Add 5 shield",                       			// description
+			"shield_pack_5",								// item id
+			5,												// number of currencies in the pack
+			SHIELD_CURRENCY_ITEM_ID,                        // the currency associated with this pack
+			new PurchaseWithMarket(FIVESHIELD_PACK_PRODUCT_ID, 0.99)
+			);
+		
+		/** Virtual Goods **/
+		
+		/** Virtual Categories **/
+
+		/** LifeTimeVGs **/
+		// Note: create non-consumable items using LifeTimeVG with PuchaseType of PurchaseWithMarket
+		public static VirtualGood NO_ADS_LTVG = new LifetimeVG(
+			"No Ads", 														// name
+			"No More Ads!",				 									// description
+			"no_ads",														// item id
+			new PurchaseWithMarket(NO_ADS_LIFETIME_PRODUCT_ID, 4.99));	// the way this virtual good is purchased
+	}
+	
+}
